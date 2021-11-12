791596007826dec875025234d6439ea4d20145da
Marc SIBERT <marc@sibert.fr>
2021-11-12 13:49:42 +0100

    Init

---
+++ PyLeanIX/__init__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class LeanIX:
+
+    def __init__(self, instance: str, api_token: str, proxies=None):
+        """Constructor"""
+        self._logger = logging.getLogger(__name__)
+        self._session = requests.session()
+        if proxies:
+            self._session.proxies.update(proxies)
+        self._instance = instance
+        self._access_token = self._get_api_token(api_token)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return
+
+    def _get_api_token(self, api_token: str) -> str:
+        """Return access token found using API_TOKEN"""
+        response = self._session.post(
+            self._instance + '/services/mtm/v1/oauth2/token',
+            auth=HTTPBasicAuth('apitoken', api_token),
+            data={'grant_type': 'client_credentials'}
+        )
+        response.raise_for_status()
+        return response.json()['access_token']
+
+    def request(self, _class: str, params: dict, cur: str = None) -> (str, object):
+        params['cursor'] = cur
+        response = self._session.get(
+            self._instance + '/services/pathfinder/v1/' + _class,
+            headers={
+                'authorization': 'Bearer ' + self._access_token,
+                'accept': 'application/json',
+                'cursor': cur
+            },
+            params=params
+        )
+        return response.json()['cursor'], response.json()['data']
---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from pprint import pprint
+
+import PyLeanIX
+from secrets import API_TOKEN, INSTANCE, PROXIES
+
+if __name__ == '__main__':
+    with PyLeanIX.LeanIX(INSTANCE, API_TOKEN, PROXIES) as app:
+        cursor = None
+        data = list()
+        while True:
+            c, d = app.request('factSheets', {
+                'type': 'Application',
+                'archivedOnly': False,
+                'pageSize': 50,
+                'permissions': False,
+                'completion': True,
+                'documents': True,
+                'tags': True,
+                'subscriptions': True,
+                'constrainingRelations': True
+            }, cursor)
+            if d:
+                data += d
+                cursor = c
+            else:
+                break
+
+        for line in data:
+            pprint(line, indent=1, width=132, sort_dicts=False)
