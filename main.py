d2364241113526437cc2f291298ffc0d9dfec4e3
Marc SIBERT <marc@sibert.fr>
2021-11-13 12:54:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,72 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from pprint import pprint
+import pprint
 
 import PyLeanIX
-from secrets import API_TOKEN, INSTANCE, PROXIES
+from PyLeanIX import LeanIX
+from my_secrets import API_TOKEN, INSTANCE, PROXIES
 
-if __name__ == '__main__':
-    with PyLeanIX.LeanIX(INSTANCE, API_TOKEN, PROXIES) as app:
-        cursor = None
-        data = list()
-        while True:
-            c, d = app.request('factSheets', {
-                'type': 'Application',
-                'archivedOnly': False,
-                'pageSize': 50,
-                'permissions': False,
-                'completion': True,
-                'documents': True,
-                'tags': True,
-                'subscriptions': True,
-                'constrainingRelations': True
-            }, cursor)
-            if d:
-                data += d
-                cursor = c
-            else:
-                break
+from flask import Flask, escape, g, current_app
 
-        for line in data:
-            pprint(line, indent=1, width=132, sort_dicts=False)
+
+def get_leanix():
+    with app.app_context() as ctx:
+        if 'leanix' not in ctx:
+            ctx.leanix = PyLeanIX.LeanIX(INSTANCE, API_TOKEN, PROXIES)
+        return ctx.leanix;
+
+
+app = Flask(__name__)
+# app.leanix = PyLeanIX.LeanIX(INSTANCE, API_TOKEN) #, PROXIES)
+# print("Leanix")
+
+
+@app.route('/')
+def index():
+    """Retourne la page d'accueil du site avec la liste générale des applications"""
+    body: str = '<h1>Welcome to LeanIX interface app !</h1>'
+    body += "<ul>"
+
+    cursor = None
+    while True:
+        c, d = get_leanix().request('factSheets', {
+            'type': 'Application',
+            'archivedOnly': False,
+            'names': False,
+            'permissions': False,
+            'completion': False,
+            'documents': False,
+            'tags': False,
+            #            'fields': False,
+            #            'subscriptions': False,
+            #            'constrainingRelations': False
+        }, cursor)
+        if d:
+            # print(f"Taille de la réponse : {len(d)}")
+            for line in d:
+                body += "<li>\n"
+                body += f"<h2><a href=\'{INSTANCE + '/itmlive/factsheet/Application/' + line['id']}\'>" \
+                        f"{line.get('display_name', line['name'])}" \
+                        "</a></h2>"
+                body += f"<p>{line.get('description', '<i>Pas de description</i>')}</p>".replace('\n', '<br/>')
+
+                if line.get('fields'):
+                    body += "<h3>Champs</h3><ul>"
+                    for f in line['fields']:
+                        if f['name'] == 'externalId':
+                            body += f"<li>Code Application : {f['data']['externalId']}</li>"
+                    body += "</ul>"
+
+                body += f'<tt>{escape(pprint.pformat(line, indent=1, width=132, sort_dicts=False))}</tt><br/>'
+                body += "</li>\n"
+            cursor = c
+        else:
+            break
+
+    body += "</ul>"
+    return body
+
+
+if __name__ == '__main__' or __name__ == 'main':
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
